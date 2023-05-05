132afb1847e585a227e6c6735ad23059ae588b2b
abcd0112 <132420472+abcd0112@users.noreply.github.com>
2023-05-05 12:23:34 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -10,24 +10,53 @@ Step = 0
 basic.forever(function () {
     basic.showNumber(Step)
     basic.showLeds(`
-        . . . # #
-        . . . # #
-        . . # # #
-        . # # # #
-        # # # # #
-        `)
-    basic.showLeds(`
-        . . # # .
-        . . # # .
-        . . # # .
-        . # # # .
-        # # # # .
-        `)
-    basic.showLeds(`
-        . # # . .
-        . # # . .
-        . # # . .
-        . # # . .
+        # . . . .
         # # # . .
+        # . . . .
+        # . . . .
+        . # . . .
         `)
+    basic.showLeds(`
+        . # . . .
+        # # # # .
+        . # . . .
+        . # . . .
+        # . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . . # .
+        . # # # #
+        . . . # .
+        . . . # .
+        . . # . #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . # # #
+        . . . . #
+        . . . . #
+        . . . # .
+        `)
+})
+basic.forever(function () {
+    if (true) {
+        if (Step == 50) {
+            music.playMelody("C5 E G D G F A F ", 500)
+        }
+    } else if (false) {
+        if (Step == 25) {
+            music.playMelody("C5 E G D G F A F ", 500)
+        }
+    } else {
+        if (Step == 15) {
+            music.playMelody("C5 E G D G F A F ", 500)
+        }
+    }
 })
